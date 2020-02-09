55727261e0d50896f02030a33d47e8d58eba3656
saksham0804 <saksham.0804@gmail.com>
2020-02-09 16:23:24 +0530

    Cart Adapter

--- app/src/main/java/com/ntas/FarmUncle/Product.java
+++ app/src/main/java/com/ntas/FarmUncle/Product.java
@@ -3,5 +3,34 @@ package com.ntas.FarmUncle;
 public class Product {
     private int id;
     private String title;
+    private double price,rating;
+    private int image;
 
+    public Product(int id, String title, double price, double rating, int image) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.rating = rating;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getImage() {
+        return image;
+    }
 }
---
+++ app/src/main/java/com/ntas/FarmUncle/ProductAdapter.java
@@ -0,0 +1,4 @@
+package com.ntas.FarmUncle;
+
+public class ProductAdapter {
+}
