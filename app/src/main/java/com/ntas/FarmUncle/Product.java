86b8c499ea97e0e484d5842cf2f6a53143a50df9
saksham0804 <saksham.0804@gmail.com>
2020-02-09 16:44:09 +0530

    Cart Adapter

--- app/src/main/java/com/ntas/FarmUncle/Product.java
+++ app/src/main/java/com/ntas/FarmUncle/Product.java
@@ -1,16 +1,18 @@
 package com.ntas.FarmUncle;
 
 public class Product {
-    private int id;
+    private int id,qty;
     private String title;
-    private double price,rating;
+    private double price,rating,cuttedPrice;
     private int image;
 
-    public Product(int id, String title, double price, double rating, int image) {
+    public Product(int id, int qty, String title, double price, double rating, double cuttedPrice, int image) {
         this.id = id;
+        this.qty = qty;
         this.title = title;
         this.price = price;
         this.rating = rating;
+        this.cuttedPrice = cuttedPrice;
         this.image = image;
     }
 
@@ -18,19 +20,55 @@ public class Product {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public double getPrice() {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public double getRating() {
         return rating;
     }
 
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getCuttedPrice() {
+        return cuttedPrice;
+    }
+
+    public void setCuttedPrice(double cuttedPrice) {
+        this.cuttedPrice = cuttedPrice;
+    }
+
     public int getImage() {
         return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
     }
 }
--- app/src/main/java/com/ntas/FarmUncle/ProductAdapter.java
+++ app/src/main/java/com/ntas/FarmUncle/ProductAdapter.java
@@ -1,4 +1,51 @@
 package com.ntas.FarmUncle;
 
-public class ProductAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProdctViewHolder> {
+
+    private Context context;
+    private List<Product> productList;
+
+    public ProductAdapter(Context context, List<Product> productList) {
+        this.context = context;
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProdctViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View view = layoutInflater.inflate(R.layout.item_card_cart,null);
+        return new ProdctViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProdctViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class ProdctViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView price,cuttedPrice,qty,title,rating;
+        public ProdctViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.product_image);
+        }
+    }
 }
